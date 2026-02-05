227d7bd7e85216866a3ba333fd1c433ca494f099
Michel Edet <micheledet08@gmail.comgit config --global --listqgit config --global user.namegit config --global user.email gitgi     hhd-9enjckamsxcomQZZZZQQQQQQQQQQQQQDDYDGsfcuuscvyiwcuowcduowehiowefiwefnpefwnkcdnkcdpwcdnkwcdnklcdnkcdnklcdnk30h8dodiodjncdnlscnlwnownowuiwoixwxxhhxwshxheyetfgxvxvxsvstr76fvivvivik./ZZXXCCCVXVVNMVNCVMMXVVXVgiioupijopko[jk3if0snisnc dnvdndicc-OO9DN5G MINGW64 ~$ git config --global user.name Michel>
2026-02-05 09:08:42 +0100

    Improve portfolio layout and spacing

---
+++ script.js/script.js
@@ -0,0 +1,10 @@
+
+     const menuToogle =
+   document.getElementById("hamburger");
+    const navLinks =
+   document.getElementById(".nav-links");
+    
+    Hamburger.addEventListener("click",()=>{
+       navLinks.classList.toogle("show");
+    });
+    
