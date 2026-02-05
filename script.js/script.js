204a29baa79787bdd01be8c9ce8f26d11e15adfb
Michel Edet <micheledet08@gmail.comgit config --global --listqgit config --global user.namegit config --global user.email gitgi     hhd-9enjckamsxcomQZZZZQQQQQQQQQQQQQDDYDGsfcuuscvyiwcuowcduowehiowefiwefnpefwnkcdnkcdpwcdnkwcdnklcdnkcdnklcdnk30h8dodiodjncdnlscnlwnownowuiwoixwxxhhxwshxheyetfgxvxvxsvstr76fvivvivik./ZZXXCCCVXVVNMVNCVMMXVVXVgiioupijopko[jk3if0snisnc dnvdndicc-OO9DN5G MINGW64 ~$ git config --global user.name Michel>
2026-02-05 09:23:47 +0100

    improve poetfolio layout and design

--- script.js/script.js
+++ script.js/script.js
@@ -1,10 +1,9 @@
 
-     const menuToogle =
-   document.getElementById("hamburger");
-    const navLinks =
-   document.getElementById(".nav-links");
-    
-    Hamburger.addEventListener("click",()=>{
-       navLinks.classList.toogle("show");
-    });
-    
+    const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("nav-links");
+const navbar = document.querySelector(".navbar");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
+});
+
