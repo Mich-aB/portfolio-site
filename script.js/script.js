1a548e0871827095e2b0981f19f7c461aae19e67
Michel Edet <micheledet08@gmail.comgit config --global --listqgit config --global user.namegit config --global user.email gitgi     hhd-9enjckamsxcomQZZZZQQQQQQQQQQQQQDDYDGsfcuuscvyiwcuowcduowehiowefiwefnpefwnkcdnkcdpwcdnkwcdnklcdnkcdnklcdnk30h8dodiodjncdnlscnlwnownowuiwoixwxxhhxwshxheyetfgxvxvxsvstr76fvivvivik./ZZXXCCCVXVVNMVNCVMMXVVXVgiioupijopko[jk3if0snisnc dnvdndicc-OO9DN5G MINGW64 ~$ git config --global user.name Michel>
2026-02-09 18:21:41 +0100

    change inscript.js and update of website

--- script.js/script.js
+++
@@ -1,9 +0,0 @@
-
-    const hamburger = document.getElementById("hamburger");
-const navLinks = document.getElementById("nav-links");
-const navbar = document.querySelector(".navbar");
-
-hamburger.addEventListener("click", () => {
-  navLinks.classList.toggle("show");
-});
-
